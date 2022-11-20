20b87847d170432f50d71f13b848089446c4d9e4
narapat <top.narapat@gmail.com>
2022-11-20 13:34:57 +0700

    add t.Run

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -3,32 +3,24 @@ package sum
 import "testing"
 
 func TestSum(t *testing.T) {
-	// t.Log("testing sum")
+	t.Run("should return 3 when 1 and 2", func(t *testing.T) {
+		got := sum(1, 2)
+		if got != 3 {
+			t.Errorf("sum(1, 2) = %d; want 3", got)
+		}
+	})
 
-	// if 1 != 2 {
-	// 	t.Error("error 1 != 2")
-	// }
+	t.Run("should return 1 when 1 and 0", func(t *testing.T) {
+		got := sum(1, 0)
+		if got != 1 {
+			t.Errorf("sum(1, 0) = %d; want 1", got)
+		}
+	})
 
-	// if 1 != 1 {
-	// 	t.Error("error 1 != 2")
-	// }
-
-	got := sum(1, 2)
-	if got != 3 {
-		t.Errorf("sum(1, 2) = %d; want 3", got)
-	}
-}
-
-func TestSumShouldReturn1WhenInput1and0(t *testing.T) {
-	got := sum(1, 0)
-	if got != 1 {
-		t.Errorf("sum(1, 0) = %d; want 1", got)
-	}
-}
-
-func TestSumShouldReturnMinus2WhenInputMinus1andMinus1(t *testing.T) {
-	got := sum(-1, -1)
-	if got != -2 {
-		t.Errorf("sum(-1, -1) = %d; want -2", got)
-	}
+	t.Run("should return -2 when -1 and -1", func(t *testing.T) {
+		got := sum(-1, -1)
+		if got != -2 {
+			t.Errorf("sum(-1, -1) = %d; want -2", got)
+		}
+	})
 }
